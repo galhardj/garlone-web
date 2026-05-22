7039d553ae6c2336f4717444e52da26fa51cf65a
Galih <galihhad@gmail.com>
2026-05-22 15:48:38 +0700

    Use explicit props for ImageNext component

--- src/components/common/ImageNext.tsx
+++ src/components/common/ImageNext.tsx
@@ -1,12 +1,36 @@
-import Image, { ImageProps } from "next/image";
+import Image, { type ImageProps } from "next/image";
 
-type AppImage = Omit<ImageProps, "src"> & {
+type ImageNextProps = {
   src: string;
+  alt: string;
+  className?: string;
+  fill?: boolean;
+  priority?: boolean;
+  sizes?: ImageProps["sizes"];
 };
 
 // TODO: consider; div wrapping, 'sizes' att
-const ImageNext = ({ ...props }: AppImage) => {
-  return <Image fill unoptimized={props.src.endsWith(".gif")} {...props} />;
+const ImageNext = ({ src, fill = true, ...props }: ImageNextProps) => {
+  return (
+    <Image
+      src={src}
+      fill={fill}
+      unoptimized={src.endsWith(".gif")}
+      {...props}
+    />
+  );
 };
 
 export default ImageNext;
+
+// export type ImageProps = {
+//   sources?: Partial<{ [_T in ScreenNames]: string }>;
+//   img: Img;
+//   imageWidths?: object;
+//   defaultWidth?: number;
+//   priority?: boolean;
+//   ariaHidden?: boolean;
+//   alt?: string;
+//   useProductWrapper?: boolean;
+//   objectFit?: ImgElementStyle["objectFit"];
+// };
