9ede5170f9a637314b17c934e11809279bc997a8
Galih <galihhad@gmail.com>
2026-05-25 21:03:08 +0700

    Add new font family and style in body tag

--- src/app/(pages)/layout.tsx
+++ src/app/(pages)/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Noto_Sans_Symbols } from "next/font/google";
 import "@/src/styles/globals.css";
 
 export const metadata: Metadata = {
@@ -6,14 +7,21 @@ export const metadata: Metadata = {
   description: "A minimal Next.js homepage with static content",
 };
 
+const webbyFontFamily = Noto_Sans_Symbols({
+  subsets: ["latin"],
+  variable: "--font-family-webby",
+});
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="en" className={`${webbyFontFamily.variable}`}>
+      <body className="font-family-webby text-foreground antialiased">
+        {children}
+      </body>
     </html>
   );
 }
