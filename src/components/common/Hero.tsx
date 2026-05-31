e822d19d46dc2b00e69322d685a5e3aa96833b81
Galih <galihhad@gmail.com>
2026-05-31 15:46:05 +0700

    Update Tablet view

--- src/components/common/Hero.tsx
+++ src/components/common/Hero.tsx
@@ -11,8 +11,8 @@ const Hero = ({
   link,
 }: HeroBannerProps) => {
   return (
-    <div className="relative h-screen w-full">
-      <div className="absolute inset-0 max-h-[75vh]">
+    <div className="relative h-[57vh] w-full lg:h-screen">
+      <div className="absolute inset-0 max-h-[37vh] lg:max-h-[75vh]">
         <ImageNext
           src={image.src}
           alt={image.alt}
@@ -21,10 +21,10 @@ const Hero = ({
         />
       </div>
       <div className="absolute bottom-8 left-1/2 w-[80%] -translate-x-1/2">
-        <div className="bg-background absolute -inset-8 lg:-inset-12" />
+        <div className="bg-background absolute -inset-12" />
         <div className="text-foreground relative flex flex-col items-center gap-4">
           <h1>{title}</h1>
-          <p className="max-w-lg text-center">{description}</p>
+          <p className="max-w-2xl text-center lg:max-w-lg">{description}</p>
           <LinkButton
             href={link.url}
             buttonColor="transparent"
