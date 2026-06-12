e3fb99b7fc0684deffcc1068284b0edd519e785b
Galih <galihhad@gmail.com>
2026-06-12 22:40:53 +0700

    Move Contentful token to header request

--- src/lib/api/contentful.ts
+++ src/lib/api/contentful.ts
@@ -2,10 +2,13 @@ import httpClient from "@/src/lib/http-client";
 import { Banner } from "@/src/type/contentful";
 
 export const fetchContentful = async () => {
-  const contentfulBanner = `https://cdn.contentful.com/spaces/${process.env.CONTENTFUL_SPACE}/environments/master/entries/3h4tEEKIoBT03odMTANIoK?access_token=${process.env.CONTENTFUL_TOKEN}`;
+  const contentfulBanner = `${process.env.CONTENTFUL_DOMAIN_CDN}/spaces/${process.env.CONTENTFUL_SPACE}/environments/${process.env.CONTENTFUL_ENV_PROD}/entries/3h4tEEKIoBT03odMTANIoK`;
 
   return await httpClient<Banner>(contentfulBanner, {
     method: "GET",
     renderMode: "SSG",
+    headers: {
+      Authorization: `Bearer ${process.env.CONTENTFUL_TOKEN}`,
+    },
   });
 };
