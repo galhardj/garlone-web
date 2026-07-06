25343e82d7814ba59774eb7fb9ac90214e91759c
Galih <galihhad@gmail.com>
2026-07-06 08:52:39 +0700

    Extract config scripts from package.json to separate files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,10 @@
 import js from "@eslint/js";
+import storybook from "eslint-plugin-storybook";
 
 export default [
   {
     ignores: [".next/**", "node_modules/**", "**/*.ts", "**/*.tsx"],
   },
   js.configs.recommended,
+  ...storybook.configs["flat/recommended"],
 ];
---
+++ prettier.config.mjs
@@ -0,0 +1,14 @@
+export default {
+  plugins: [
+    "@trivago/prettier-plugin-sort-imports",
+    "prettier-plugin-tailwindcss",
+  ],
+  importOrder: [
+    "^(react|next)(.*)$",
+    "<THIRD_PARTY_MODULES>",
+    "^@/(.*)$",
+    "^[./]",
+  ],
+  importOrderParserPlugins: ["typescript", "jsx", "decorators-legacy"],
+  importOrderSortSpecifiers: true,
+};
